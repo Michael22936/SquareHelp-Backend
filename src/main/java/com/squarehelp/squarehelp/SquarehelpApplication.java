f4416cfd35e428066e7fcb3f66a1851fc33a6c17
Matt Lopez <matthew.lopez4011@gmail.com>
2019-11-15 14:15:22 -0600

    Created spring project and added git ignore file

---
+++ src/main/java/com/squarehelp/squarehelp/SquarehelpApplication.java
@@ -0,0 +1,13 @@
+package com.squarehelp.squarehelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquarehelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquarehelpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/squarehelp/squarehelp/SquarehelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squarehelp.squarehelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquarehelpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
