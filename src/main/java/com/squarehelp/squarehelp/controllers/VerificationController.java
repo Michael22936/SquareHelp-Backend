7c8aa91e7e468c8a349d40924691582f24dd6c88
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-11-21 18:03:25 -0600

    feat: added initial verification controller

---
+++ src/main/java/com/squarehelp/squarehelp/controllers/VerificationController.java
@@ -0,0 +1,28 @@
+package com.squarehelp.squarehelp.controllers;
+
+import com.squarehelp.squarehelp.repositories.SmokerInfoRepository;
+import com.squarehelp.squarehelp.repositories.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VerificationController {
+    private final UserRepository userDao;
+
+    public VerificationController(UserRepository userDao) {
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/verify/message")
+    public String getVerifyMessageView(Model model){
+
+        model.addAttribute("users", userDao);
+
+        return "/message";
+    }
+
+
+
+
+}
