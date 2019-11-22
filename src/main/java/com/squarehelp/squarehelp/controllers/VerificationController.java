07a26c440e1175bf3f24856179ff6241aa9de12c
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-11-21 20:06:56 -0600

    feat: update view with initial form

--- src/main/java/com/squarehelp/squarehelp/controllers/VerificationController.java
+++ src/main/java/com/squarehelp/squarehelp/controllers/VerificationController.java
@@ -1,27 +1,32 @@
 package com.squarehelp.squarehelp.controllers;
 
+import com.squarehelp.squarehelp.models.SmokerInfo;
 import com.squarehelp.squarehelp.repositories.SmokerInfoRepository;
 import com.squarehelp.squarehelp.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class VerificationController {
+    private final SmokerInfoRepository smokeDao;
     private final UserRepository userDao;
 
-    public VerificationController(UserRepository userDao) {
+    public VerificationController(SmokerInfoRepository smokeDao, UserRepository userDao) {
+        this.smokeDao = smokeDao;
         this.userDao = userDao;
     }
 
-    @GetMapping("/verify/message")
-    public String getVerifyMessageView(Model model){
+    @GetMapping("/verification/{user_id}")
+    public String getVerifyMessageView(Model model, @PathVariable long user_id){
+        SmokerInfo smokerInfo = smokeDao.getOne(user_id);
 
-        model.addAttribute("users", userDao);
 
-        return "/message";
+        model.addAttribute("users", userDao.getOne(user_id));
+        model.addAttribute("smoke", smokerInfo);
+
+        return "verification";
     }
-
 
 
 }
