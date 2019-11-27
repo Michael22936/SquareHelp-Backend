842cddf98217ce17a9986254353b67ccd56ebc0a
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-11-26 18:39:43 -0600

    almost complete message notifications

--- src/main/java/com/squarehelp/squarehelp/services/NotificationServices.java
+++ src/main/java/com/squarehelp/squarehelp/services/NotificationServices.java
@@ -3,13 +3,20 @@ package com.squarehelp.squarehelp.services;
 import com.squarehelp.squarehelp.models.Notification;
 import com.squarehelp.squarehelp.models.User;
 import com.squarehelp.squarehelp.repositories.NotificationRepository;
+import com.squarehelp.squarehelp.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationServices {
 
     private final NotificationRepository notiDao;
-    private final user
+    private final UserRepository userDao;
+
+    public NotificationServices(NotificationRepository notiDao, UserRepository userDao){
+        this.notiDao = notiDao;
+        this.userDao = userDao;
+    }
 
     /*
      *   Creates notification in the notification table.
@@ -21,12 +28,16 @@ public class NotificationServices {
      *       "veri" - For smoke request verifications
      */
     public void createNotification(String username, Long uid, String type) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         // Verify all fields are there before running
         if (username == null || uid == null || type == null) {
+            System.out.println("hello");
             return;
         } else if (username == null && uid == null && type == null) {
+            System.out.println("goodbye");
             return;
         } else {
+            System.out.println("it worked");
             Notification n = new Notification();
 
             User recipient = userDao.findByUsername(username);
@@ -35,7 +46,9 @@ public class NotificationServices {
                 case ("msg"):
                     n.setRecipient_user_id(String.valueOf(recipient.getId()));
                     n.setOriginator_user_id(String.valueOf(uid));
+                    n.setUser_noti(user);
                     n.setNotification("You have a new message from " + username);
+                    System.out.println("msg");
                     break;
                 case ("veri"):
                     n.setRecipient_user_id(String.valueOf(recipient.getId()));
@@ -43,6 +56,7 @@ public class NotificationServices {
                     n.setNotification("You have a smoke verification request from " + username);
                     break;
                 default:
+                    System.out.println("default");
                     return;
             }
 
