8f15901ad58c3ba613e8f6b5d36742d850340850
yaelBrown <yaelrbrown@gmail.com>
2019-12-01 00:27:44 -0600

    refactored js file and removed filestack partial.

--- src/main/resources/static/js/util/filestack-impl.js
+++ src/main/resources/static/js/util/filestack-impl.js
@@ -22,14 +22,13 @@ const options = {
     uploadInBackground: false
 };
 
-let picker = client.picker(options);
+const picker = client.picker(options);
 
 // DOM elements
-let form = document.getElementById('photo');
-let fileInput = document.getElementById('fileupload');
-let pictureUploadbtn = document.getElementById('picker');
-// let nameBox = document.getElementById('nameBox');
-let editProfileImg = document.getElementById('profileImg');
+const form = document.getElementById('photo');
+const fileInput = document.getElementById('fileupload');
+const pictureUploadbtn = document.getElementById('picker');
+const editProfileImg = document.getElementById('profileImg');
 
 // // Add our event listeners
 pictureUploadbtn.addEventListener('click', function (e) {
@@ -37,4 +36,4 @@ pictureUploadbtn.addEventListener('click', function (e) {
     picker.open();
 });
 
-var photo = "";
+let photo = "";
