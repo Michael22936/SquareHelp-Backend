ad65dde787a62a311ea43f5fc057b2d294861835
Matt Lopez <matthew.lopez4011@gmail.com>
2019-12-01 22:24:15 -0600

    Fixing stats cards layout

--- src/main/resources/static/js/dashboard/dashboard.js
+++ src/main/resources/static/js/dashboard/dashboard.js
@@ -65,7 +65,76 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-        // ================================== 2nd chart
+        // ================================== 1st chart (Community Stats)
+
+        var options = {
+            chart: {
+                height: 350,
+                type: 'radialBar',
+            },
+            plotOptions: {
+                radialBar: {
+                    offsetY: -10,
+                    startAngle: 0,
+                    endAngle: 270,
+                    hollow: {
+                        margin: 5,
+                        size: '30%',
+                        background: 'transparent',
+                        image: undefined,
+                    },
+                    dataLabels: {
+                        name: {
+                            show: false,
+
+                        },
+                        value: {
+                            show: false,
+                        }
+                    }
+                }
+            },
+            colors: ['#1ab7ea', '#0084ff', '#39539E', '#0077B5'],
+            series: [TotalUsersAvgPointsFixed,TotalUsersAvgSmokeFreeDays,TotalUsersAvgCigSavings],
+            labels: ['Average Points Earned', 'Average Days Smoke Free', 'Average Savings in USD'],
+            legend: {
+                show: true,
+                floating: true,
+                fontSize: '16px',
+                position: 'left',
+                offsetX: 160,
+                offsetY: 10,
+                labels: {
+                    useSeriesColors: true,
+                },
+                markers: {
+                    size: 0
+                },
+                formatter: function(seriesName, opts) {
+                    return seriesName + ":  " + opts.w.globals.series[opts.seriesIndex]
+                },
+                itemMargin: {
+                    horizontal: 1,
+                }
+            },
+            responsive: [{
+                breakpoint: 480,
+                options: {
+                    legend: {
+                        show: false
+                    }
+                }
+            }]
+        }
+
+        var chart = new ApexCharts(
+            document.querySelector("#avgPointChart1"),
+            options
+        );
+
+        chart.render();
+
+        // ================================== 2nd chart (Users Stats)
 
         var options = {
             chart: {
