2ad926cedaf993279d511c24a001449ee866f344
Michael McDonnell <Michael.McDonnell067@gmail.com>
2020-07-13 23:01:14 -0500

    working on fixing a verification error. maybe it will fix the error page problem

--- src/main/java/com/squarehelp/squarehelp/controllers/CommunityController.java
+++ src/main/java/com/squarehelp/squarehelp/controllers/CommunityController.java
@@ -3,9 +3,12 @@ package com.squarehelp.squarehelp.controllers;
 import com.squarehelp.squarehelp.models.User;
 import com.squarehelp.squarehelp.repositories.SmokerInfoRepository;
 import com.squarehelp.squarehelp.repositories.UserRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 public class CommunityController {
     private final UserRepository userDao;
@@ -17,9 +20,10 @@ public class CommunityController {
     }
 
     @GetMapping("/community")
-    public String CommunityPage(Model model){
+    @ResponseBody
+    public String CommunityPage(){
 
-        return "community";
+        return "hello world";
     }
 
 }
